da95018b2a0564c984fa3e2fff474ab885712c43
it63070185 <73686836+it63070185@users.noreply.github.com>
2022-11-22 04:19:53 +0700

    8

--- CountPage.js
+++ CountPage.js
@@ -1,13 +1,19 @@
 const back1 = document.querySelector(".door_in")
-
+const door_last = document.querySelector(".door4")
 function back(){
     var link1 = window.location.href
     var MyArry1 = link1.split("?")
     var num = MyArry1[1]
-    if(num == "4"){
-        window.location.href ="home.html?0"
+    if(num == ""){
+        window.location.href ="home.html?"
+        door_last.classList.remove("preloader-hidden")
+        door_last.classList.add("preloader-show")
+        
     }else{
         window.location.href ="home.html?"+num
+        door_last.classList.remove("preloader-show")
+        door_last.classList.add("preloader-hidden")
+        
     }
 }
 

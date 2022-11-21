4c74c990c398c1ce4a252c87d0ba7c04ae9a08d1
panithan-kongthong <73686785+panithan-kongthong@users.noreply.github.com>
2022-11-22 04:54:26 +0700

    back End

--- CountPage.js
+++ CountPage.js
@@ -1,19 +1,19 @@
 const back1 = document.querySelector(".door_in")
-const door_last = document.querySelector(".door4")
+
 function back(){
     var link1 = window.location.href
     var MyArry1 = link1.split("?")
     var num = MyArry1[1]
+    // console.log("Show")
     if(num == ""){
+        console.log("Show")
         window.location.href ="home.html?"
-        door_last.classList.remove("preloader-hidden")
-        door_last.classList.add("preloader-show")
         
     }else{
+        console.log("NotShow")
+        // door_last.classList.remove("preloader-show")
+        // door_last.classList.add("preloader-hidden")
         window.location.href ="home.html?"+num
-        door_last.classList.remove("preloader-show")
-        door_last.classList.add("preloader-hidden")
-        
     }
 }
 

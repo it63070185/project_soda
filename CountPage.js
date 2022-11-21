df246a5d2727514d9885b85defb4e80d8da05dc8
panithan-kongthong <73686785+panithan-kongthong@users.noreply.github.com>
2022-11-22 03:49:39 +0700

    Link page

--- CountPage.js
+++ CountPage.js
@@ -1,6 +1,15 @@
 const back1 = document.querySelector(".door_in")
-var room
-	function back(){
-		
-		window.location.href ="home.html"
-	}
+
+function back(){
+    var link1 = window.location.href
+    var MyArry1 = link1.split("?")
+    var num = MyArry1[1]
+    if(num == "4"){
+        window.location.href ="home.html?0"
+    }else{
+        window.location.href ="home.html?"+num
+    }
+}
+
+
+
